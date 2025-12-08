ea387fed793343dee7f998eee732defdd153550b
Muhammad Rafi Indra <mrafiindra.contact@gmail.com>
2025-12-08 13:02:42 +0700

    feature: Added method for page querying with filters to NotificationRepository

--- backend/src/main/java/com/ilemgroup/internship/taskmanager/backend/repository/NotificationRepository.java
+++ backend/src/main/java/com/ilemgroup/internship/taskmanager/backend/repository/NotificationRepository.java
@@ -2,7 +2,11 @@ package com.ilemgroup.internship.taskmanager.backend.repository;
 
 import com.ilemgroup.internship.taskmanager.backend.entity.Notification;
 import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<@NonNull Notification, @NonNull Long> {
+    Page<@NonNull Notification> findAll(Specification<@NonNull Notification> specification, Pageable pageable);
 }
