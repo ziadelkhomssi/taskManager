6cf5f77e70ec42b63bc5c203d2d1289aa9cf9ec8
Muhammad Rafi Indra <mrafiindra.contact@gmail.com>
2025-12-11 19:54:33 +0700

    fix: Disabled anonymous requests to backend

--- backend/src/main/java/com/ilemgroup/internship/taskmanager/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/com/ilemgroup/internship/taskmanager/backend/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
@@ -15,6 +16,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
+                .anonymous(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated()
                 )
