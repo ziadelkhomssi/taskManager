c7c96f32e6ba36742cc1a39128ccd6cff5cc1839
Muhammad Rafi Indra <mrafiindra.contact@gmail.com>
2025-12-14 23:40:14 +0700

    feature: Added basic static layout of toolbar and sidenav for frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenav, MatSidenavContainer, MatSidenavContent, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatNavList } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    MatSidenavContainer, 
+    MatSidenav, 
+    MatSidenavModule,
+    MatSidenavContent, 
+    MatToolbar,
+    MatIcon,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  logout() {
+
+  }
 }
