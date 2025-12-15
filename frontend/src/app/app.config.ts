f4d6b060399c2ed06ec0dcb4a021cce0f9ecef7c
Muhammad Rafi Indra <mrafiindra.contact@gmail.com>
2025-12-16 01:04:21 +0700

    feature: Created environments for frontend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,12 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { provideNgOpenapi } from './core/ng-openapi';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient()
+    provideHttpClient(),
+    provideNgOpenapi({
+      basePath: environment.API_URL
+    })
   ]
 };
