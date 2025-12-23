bd4a96a9068e72f7b1ed967c3cf0caeeea0b3ab4
Muhammad Rafi Indra <mrafiindra.contact@gmail.com>
2025-12-23 19:20:36 +0700

    feature: Added user profile

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,29 +1,61 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectorRef, Component, signal } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSidenav, MatSidenavContainer, MatSidenavContent, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
 import { MatNavList } from '@angular/material/list';
+import { UserDetails, UserSummary } from './core/ng-openapi';
+import { UserService } from './core/services/user-service';
+import { FallbackImage } from './shared/directive/fallback-image/fallback-image';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet, 
+    RouterOutlet,
+    RouterLink, 
     MatSidenavContainer, 
     MatSidenav, 
     MatSidenavModule,
     MatSidenavContent, 
     MatToolbar,
     MatIcon,
+    FallbackImage
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
+  
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
+
+  clientUserSummary: UserSummary = {
+    id: "def456",
+    name: "Jane Developer",
+  }
+
+  ngOnInit() {
+    this.loadClientUserSummary();
+  }
+
+  loadClientUserSummary() {
+    this.userService.getClientSummary().subscribe({
+      next: (response) => {
+        this.clientUserSummary = response;
+        this.changeDetectorRef.detectChanges();
+      },
+      error: (error) => {
+        console.error('Could not load client user!', error);
+      }
+    });
+  }
 
   logout() {
-
+    console.log("woah!! you logged out (jk)")
   }
 }
--- frontend/src/app/features/user/user-profile/user-profile.ts
+++ frontend/src/app/features/user/user-profile/user-profile.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import { UserDetails } from '../../../core/ng-openapi';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../../core/services/user-service';
+import { FallbackImage } from '../../../shared/directive/fallback-image/fallback-image';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  imports: [FallbackImage],
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.css',
 })
 export class UserProfile {
+  @Input() userDetails: UserDetails = {
+    id: "NULL",
+    name: "PLACEHOLDER",
+    job: "PLACEHOLDER",
+  };
 
+  constructor(
+    private userService: UserService,
+    private location: Location,
+    private changeDetectorRef: ChangeDetectorRef,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.loadUserDetails(params["id"])
+    });
+  }
+
+  loadUserDetails(id: string) {
+    this.userService.getDetailsById(id).subscribe({
+      next: (response) => {
+        this.userDetails = response;
+        this.changeDetectorRef.detectChanges();
+      },
+      error: (error) => {
+        console.error('Could not load user!', error);
+      }
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
