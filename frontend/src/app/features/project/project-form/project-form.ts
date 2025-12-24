2aa8fbfc328f67055da48f9d32998c4484d4ae66
Muhammad Rafi Indra <mrafiindra.contact@gmail.com>
2025-12-24 18:19:36 +0700

    feature: Added project features to frontend

--- frontend/src/app/features/project/project-form/project-form.ts
+++ frontend/src/app/features/project/project-form/project-form.ts
@@ -1,11 +1,159 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectDetails } from '../../../core/ng-openapi';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ProjectService } from '../../../core/services/project-service';
+import { DialogService } from '../../../core/services/dialog-service';
+import { MatIconModule } from '@angular/material/icon';
+import { ImageUpload } from "../../../shared/component/image-upload/image-upload";
 
 @Component({
   selector: 'app-project-form',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    ImageUpload
+  ],
   templateUrl: './project-form.html',
   styleUrl: './project-form.css',
 })
 export class ProjectForm {
+  projectForm!: FormGroup;
+  isEditMode: boolean = false;
+  projectId: number = -1;
+
+  statuses: ProjectDetails["status"][] = [
+    "PLANNED",
+    "ACTIVE",
+    "DONE",
+    "ARCHIVED"
+  ];
+
+  constructor(
+    private projectService: ProjectService,
+    private dialogService: DialogService,
+    private formBuilder: FormBuilder,
+    private location: Location,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.projectForm = this.formBuilder.group({
+      title: ["", [Validators.required, Validators.maxLength(100)]],
+      description: [""],
+      profilePicture: [null],
+      status: ["PLANNED", Validators.required]
+    });
+
+
+    this.route.params.subscribe(params => {
+      this.projectId = params["id"]
+      if (!this.projectId) {
+        return;
+      }
+      this.isEditMode = true;
+      this.loadProject(this.projectId);
+    });
+  }
+
+  loadProject(id: number) {
+    this.projectService.getDetailsById(id).subscribe({
+      next: (response) => {
+        this.projectForm.patchValue(response)
+        this.loadProjectProfilePicture(id)
+      },
+      error: (error) => {
+        console.error("Could not load project!", error);
+        this.dialogService.openErrorDialog(
+          "Could not load project! Try again later!",
+          () => { this.exitForm() }
+        );
+      }
+    });
+  }
+
+  loadProjectProfilePicture(id: number) {
+    this.projectService.getProfilePicture(id).subscribe({
+      next: (response: File | null) => {
+        if (response == null) {
+          return;
+        }
+        this.projectForm.get("profilePicture")?.setValue(response);
+      },
+      error: (error) => {
+        console.error("Could not load project profile picture!", error);
+        this.dialogService.openErrorDialog(
+          "Warning! Could not load project profile picture!", null
+        );
+      }
+    })
+  }
+
+  submit() {
+    if (this.projectForm.invalid) {
+      return;
+    }
+
+    const command = {
+      id: this.projectId == -1 ? null : this.projectId,
+      title: this.projectForm.value.title,
+      description: this.projectForm.value.description,
+      status: this.projectForm.value.status
+    };
+
+    const formData = new FormData();
+
+    formData.append(
+      "command",
+      new Blob(
+        [JSON.stringify(command)],
+        { type: "application/json" }
+      )
+    );
+
+    const profilePicture: File = this.projectForm.value.profilePicture
+    if (profilePicture) {
+      formData.append("profilePicture", profilePicture);
+    }
+
+    if (this.isEditMode) {
+
+      this.projectService.update(formData).subscribe({
+        next: () => this.exitForm(),
+        error: (error) => {
+          console.error("Could not save changes!", error)
+          this.dialogService.openErrorDialog(
+            "Could not save changes! Try again later!", null
+          );
+        }
+      });
+      return;
+    }
+
+    this.projectService.create(formData).subscribe({
+      next: () => this.exitForm(),
+      error: (error) => {
+        console.error("Could not create project!", error)
+        this.dialogService.openErrorDialog(
+          "Could not create project! Try again later!", null
+        );
+      }
+    });
+  }
+
+  exitForm() {
+    this.location.back();
+  }
 
 }
--- frontend/src/app/features/project/project-page/project-page.ts
+++ frontend/src/app/features/project/project-page/project-page.ts
@@ -1,11 +1,241 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { ProjectDetails, SprintSummary, UserSummary } from '../../../core/ng-openapi';
+import { EntityTable, TableColumn } from '../../../shared/component/entity-table/entity-table';
+import { SprintService } from '../../../core/services/sprint-service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProjectService } from '../../../core/services/project-service';
+import { FallbackImage } from '../../../shared/directive/fallback-image/fallback-image';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { PageQuery } from '../../../shared/types/types';
+import { UserListPreviewButton } from '../../../shared/component/user-list-preview-button/user-list-preview-button';
+import { UserListModal } from '../../../shared/component/user-list-modal/user-list-modal';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { ErrorDialog } from '../../../shared/component/error-dialog/error-dialog';
+import { ConfirmDialog } from '../../../shared/component/confirm-dialog/confirm-dialog';
+import { DialogService } from '../../../core/services/dialog-service';
+
+const DEFAULT_PREVIEW_PARTICIPANTS_QUERY: PageQuery = {
+  page: 0,
+  size: 3,
+  search: "",
+  filter: ""
+}
+
+const DEFAULT_SPRINTS_QUERY: PageQuery = {
+  page: 0,
+  size: 10,
+  search: "",
+  filter: ""
+}
 
 @Component({
   selector: 'app-project-page',
-  imports: [],
+  imports: [
+    UserListPreviewButton,
+    MatCardModule,
+    MatButtonModule,
+    EntityTable, 
+    FallbackImage
+  ],
   templateUrl: './project-page.html',
   styleUrl: './project-page.css',
 })
 export class ProjectPage {
+  projectDetails: ProjectDetails = {
+    id: -1,
+    title: "PLACEHOLDER",
+    description: "PLACEHOLDER",
+    status: "PLANNED",
+  };
 
+  previewParticipants: UserSummary[] = [];
+
+  sprints: SprintSummary[] = [];
+
+  columns: TableColumn<SprintSummary>[] = [
+    { columnDef: "id", header: "ID", cell: sprint => sprint.id.toString() },
+    { columnDef: "title", header: "Title", cell: sprint => sprint.title },
+    { columnDef: "status", header: "Status", cell: sprint => sprint.status },
+    { columnDef: "dueDate", header: "Due Date", cell: sprint => new Date(sprint.dueDate).toUTCString() }
+  ];
+
+  actions = [
+    {
+      label: "Update",
+      callback: (sprint: SprintSummary) => this.updateSprint(sprint)
+    },
+    {
+      label: "Delete",
+      callback: (sprint: SprintSummary) => this.deleteSprint(sprint)
+    },
+  ];
+
+  filters: string[] = [
+    "Sprint",
+    "Status",
+    "Ticket",
+    "User"
+  ];
+
+  lastSprintsQuery: PageQuery = DEFAULT_SPRINTS_QUERY;
+  pageIndex = 0;
+  pageSize = 10;
+  totalElements = 0;
+
+  projectUserFetcher = (query: PageQuery) => 
+    this.projectService.getUserSummaryList(
+      this.projectDetails.id, query
+    );
+
+  constructor(
+    private projectService: ProjectService, 
+    private sprintService: SprintService, 
+    private dialogService: DialogService,
+    private changeDetectorRef: ChangeDetectorRef,
+    private dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.loadProjectDetails(params["id"])
+    });
+  }
+
+  loadProjectDetails(id: number) {
+    this.projectService.getDetailsById(id).subscribe({
+      next: (response) => {
+        this.projectDetails = response;
+
+        this.projectUserFetcher = (query: PageQuery) => 
+          this.projectService.getUserSummaryList(
+            this.projectDetails.id, query
+          );
+
+        this.loadPreviewParticipants();
+        this.loadSprints(DEFAULT_SPRINTS_QUERY);
+
+        this.changeDetectorRef.detectChanges();
+      },
+      error: (error) => {
+        console.error("Could not load project!", error);
+        this.dialogService.openErrorDialog(
+          "Could not load project!\nPlease try again later!",
+          () => {this.router.navigate(["/"])}
+        );
+      }
+    });
+  }
+
+  loadPreviewParticipants() {
+    this.projectUserFetcher(DEFAULT_PREVIEW_PARTICIPANTS_QUERY).subscribe({
+      next: (response) => {
+        this.previewParticipants = response.content || [];
+        this.changeDetectorRef.detectChanges();
+      },
+      error: (error) => {
+        console.error("Could not load project participants!", error);
+        this.dialogService.openErrorDialog("Could not load project participants!\nPlease try again later!", null);
+      }
+    });
+  }
+
+  loadSprints(pageQuery: PageQuery) {
+    this.projectService.getSprintSummaryList(
+      this.projectDetails.id,
+      pageQuery
+    ).subscribe({
+      next: (response) => {
+        this.sprints = response.content || [];
+        this.pageIndex = response.pageable?.pageNumber || 0;
+        this.pageSize = response.pageable?.pageSize || 0;
+        this.totalElements = response.totalElements || 0;
+        this.changeDetectorRef.detectChanges();
+      },
+      error: (error) => {
+        console.error("Could not load sprints!", error);
+        this.sprints = [];
+        this.pageIndex = 0;
+        this.pageSize = 0;
+        this.totalElements = 0;
+        this.changeDetectorRef.detectChanges();
+
+        this.dialogService.openErrorDialog("Could not load sprints!\nPlease try again later!", null);
+      }
+    })
+  }
+
+  updateProject() {
+    this.router.navigate(["/project/update/" + this.projectDetails.id]);
+  }
+  deleteProject() {
+    this.dialogService.openConfirmDialog(
+      "Delete Current Project?", null, null, null,
+      () => {
+        this.projectService.deleteById(
+          this.projectDetails.id
+        ).subscribe({
+          next: () => {
+            this.router.navigate(["/"])
+          },
+          error: (error) => {
+            console.error("Could not delete project!", error);
+            this.dialogService.openErrorDialog("Could not delete project!\nPlease try again later!", null);
+          }
+    });});
+  }
+
+  onPageQuery(pageQuery: PageQuery) {
+    this.lastSprintsQuery = pageQuery;
+    this.loadSprints(pageQuery);
+  }
+
+  onRowClick(sprint: SprintSummary) {
+    this.viewSprint(sprint);
+  }
+
+  onOpenParticipantList() {
+    this.dialog.open(UserListModal, {
+      maxWidth: "600px",
+      width: "600px",
+      data: {
+        fetchUsers: this.projectUserFetcher,
+        onUserClick: (dialogRef: MatDialogRef<UserListModal>, user: UserSummary) => {
+          dialogRef.close();
+          this.router.navigate(["/user/" + user.id]);
+        },
+        description: "Project Participants"
+      }
+    });
+  }
+
+  viewSprint(sprint: SprintSummary) {
+    this.router.navigate(["/sprint/" + sprint.id]);
+  }
+  createSprint() {
+    this.router.navigate(["/sprint/create"], {queryParams: {projectId: this.projectDetails.id} });
+  }
+  updateSprint(sprint: SprintSummary) {
+    this.router.navigate(["/sprint/update/" + sprint.id], {queryParams: {projectId: this.projectDetails.id} });
+  }
+  deleteSprint(sprint: SprintSummary) {
+    this.dialogService.openConfirmDialog(
+      `Delete Sprint ${sprint.title}?`, null, null, null,
+      () => {
+        this.sprintService.deleteById(
+          sprint.id
+        ).subscribe({
+          next: () => {
+            this.loadSprints(this.lastSprintsQuery);
+          },
+          error: (error) => {
+            console.error("Could not delete sprint!", error);
+            this.dialogService.openErrorDialog(
+              "Could not delete sprint!\nPlease try again later!", null
+            );
+    }});});
+  }
 }
