e13a1fbe051399ac121bcf034deaf431a50c6a2e
Muhammad Rafi Indra <mrafiindra.contact@gmail.com>
2026-02-13 21:54:47 +0700

    dev: Added deployment for ilem private servers

--- backend/frontend/src/environments/environment.ts
+++ backend/frontend/src/environments/environment.ts
@@ -2,6 +2,6 @@ import { commonEnvironment } from "./environment.common";
 
 export const environment = {
     ...commonEnvironment,
-    API_URL: "http://localhost:8090/api",
+    API_URL: "http://10.103.8.139/api",
     enableDevelopmentRoutes: false,
 };
